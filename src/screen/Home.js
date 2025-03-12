aa402e043a04c8547f83ec4880b9e208f4806888
EduardoEinsfeldt <eduardoa.einsfeldt@gmail.com>
2025-03-12 08:53:42 -0300

    Home

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, StyleSheet, View } from 'react-native';
 
 export default function Home() {
+  const [nomeProduto, setNomeProduto] = useState('')
+  const [valorProduto, setValorProduto] = useState('')
+  const [porcentagemProduto, setPorcentagemProduto] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('../../assets/tzimisce.png')} style={styles.imagem} />
+      <TexInput
+        placeholder='Digite o nome do Produto'
+        placeholderTextColor='#880808A4'
+        style={styles.input}
+        value={nomeProduto}
+        keyboardType='default'
+      />
+      <TexInput
+        placeholder='Digite o valor do Produto'
+        placeholderTextColor='#880808A4'
+        style={styles.input}
+        value={valorProduto}
+        keyboardType='default'
+      />
+      <TexInput
+        placeholder='Digite o valor do Produto'
+        placeholderTextColor='#880808A4'
+        style={styles.input}
+        value={porcentagemProduto}
+        keyboardType='default'
+      />
+      <Button
+        title="Calcular"
+        style={styles.button}
+
+        color='black'
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +45,34 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#880808',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  imagem: {
+    resizeMode: "center",
+    width: 250,
+  },
+  input: {
+    backgroundColor: 'black',
+    width: 300,
+    borderRadius: 20,
+    color: 'red',
+    paddingLeft: 10,
+    borderWidth: 2,
+    borderColor: 'red',
+    fontSize: 20,
+    placeholderTextColor: 'red'
+  },
+  input2: {
+    backgroundColor: 'black',
+    width: 300,
+    borderRadius: 20,
+    color: 'red',
+    paddingLeft: 10,
+    borderWidth: 2,
+    borderColor: 'red',
+    fontSize: 20,
+    placeholderTextColor: 'red'
   },
 });
